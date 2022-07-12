f47b2c2a765614dd37e52d42aa123fb576caa8d1
isemenen <igor.semenenko@acadiau.ca>
2022-07-12 14:11:10 -0300

    'matlotlib_july_12'

--- rw_visual.py
+++ rw_visual.py
@@ -8,6 +8,6 @@ rw.fill_walk()
 # Plot the points in the walk.
 plt.style.use('classic')
 fig,ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, c = (1.0, 0.0, 0.0), s = 15)
+ax.scatter(rw.x_values, rw.y_values, c = (1.0, 0.0, 0.0), s = 5)
 plt.show()
 
